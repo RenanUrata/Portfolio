d79a025e7cb1b11b84e9585323983fab626db656
Renan Urata <renan.urata@gmail.com>
2021-10-12 02:54:28 -0300

    Portifolio Page update

--- src/appRoot.js
+++ src/appRoot.js
@@ -1,15 +1,26 @@
 // These two containers are siblings in the DOM
 const container = document.querySelector('.container');
 const title = document.querySelector('.title')
-imageURL = ['./portifolio/img (1).png', './portifolio/img (2).png', './portifolio/img (3).png', './portifolio/img (4).png', './portifolio/img (5).png', './portifolio/img (6).png', './portifolio/img (7).png']
+imageURL = [
+    { src: './portifolio/img (1).png', url: 'https://renanurata.github.io/' },
+    { src: './portifolio/img (3).png', url: 'https://renanurata.github.io/whatsmyip' },
+    { src: './portifolio/img (4).png', url: 'https://renanurata.github.io/keyboardDelay/' },
+    { src: './portifolio/img (5).png', url: 'https://renanurata.github.io/maxClickOneSecond/' },
+    { src: './portifolio/img (6).png', url: 'https://renanurata.github.io/mousePollingRate/' },
+    { src: './portifolio/img (7).png', url: 'https://renanurata.github.io/humanreactiontime/' },
+    { src: './portifolio/img (2).png', url: 'https://renanurata.github.io/' },]
 
 const load = () => {
     imageURL.forEach(element => {
         let imgHolder = document.createElement('img')
-        imgHolder.src = element
+        let imgLink = document.createElement('a')
+        imgLink.href = element.url
+        imgLink.appendChild(imgHolder)
+        imgHolder.src = element.src
+
         imgHolder.setAttribute('class', 'portifolio')
         imgHolder.setAttribute('loading', 'lazy')
-        container.appendChild(imgHolder)
+        container.appendChild(imgLink)
     });
 }
 
